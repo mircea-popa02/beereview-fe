ca69a7210e58b301cd4db0575c0dd2d4c2d53251
Mircea Popa <59504943+mircea-popa02@users.noreply.github.com>
2024-05-16 14:35:38 +0300

    router login register

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+      {/* Your login form goes here */}
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div>
+      <h2>Register</h2>
+      {/* Your registration form goes here */}
+    </div>
+  );
+};
+
+export default Register;
