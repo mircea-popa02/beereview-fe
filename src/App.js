1c73260066b85b527f71dd44477b98757f87963e
Mircea Popa <59504943+mircea-popa02@users.noreply.github.com>
2024-05-16 15:25:21 +0300

    first request to be

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useEffect } from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [breweries, setBreweries] = useState([]);
+  useEffect(() => {
+    // localhost:5000/breweries
+    fetch('http://localhost:5000/breweries')
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  });
+
   return (
     <div>
-      da
+      <h1>Breweries</h1>
+      <ul>
+        {breweries.map((brewery) => (
+          <li key={brewery.id}>{brewery.name}</li>
+        ))}
+      </ul>
     </div>
   );
+
 }
 
 export default App;
