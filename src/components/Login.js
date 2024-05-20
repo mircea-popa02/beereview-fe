a269325e17301949e0b9e41e2e6913874bd57bc2
RobertVlp <r.vulpe25@gmail.com>
2024-05-20 13:05:29 +0300

    Added form validation for login and register

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,35 +12,50 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await fetch('http://localhost:5000/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
-      });
-      
-      if (!response.ok) {
-        throw new Error('Invalid username or password');
+    // Check if the form inputs are empty
+    var inputs = document.querySelectorAll('input');
+    var isValid = true;
+
+    inputs.forEach((input) => {
+        if (input.type !== 'submit' && input.value.trim() === '') {
+            setError('Please fill out the ' + input.id + ' field.');
+            isValid = false;
+            input.focus();
+            return false;
+        }
+    });
+
+    if (isValid) {
+      try {
+        const response = await fetch('http://localhost:5000/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ username, password })
+        });
+        
+        if (!response.ok) {
+          throw new Error('Invalid username or password');
+        }
+
+        const data = await response.json();
+        const { token } = data;
+        localStorage.setItem('token', token);
+
+        // Redirect the user to the home page or another page
+        navigate('/home');
+      } catch (error) {
+        setError('Invalid username or password');
+        console.error('Error logging in:', error);
       }
-
-      const data = await response.json();
-      const { token } = data;
-      localStorage.setItem('token', token);
-
-      // Redirect the user to the home page or another page
-      navigate('/home');
-    } catch (error) {
-      setError('Invalid username or password');
-      console.error('Error logging in:', error);
-    }
-  };
+    };
+  }
 
   return (
-    <div class="login_box">
+    <div className="login_box">
       <img src={logo} alt="Logo"></img>
-      <form class="form" onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username:</label>
           <input
@@ -59,11 +74,11 @@ const Login = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {error && <p style={{ color: 'red', fontWeight: 500 }}>{error}</p>}
         <button type="submit">Sign In</button>
       </form>
 
-      <div class="bottom">
+      <div className="bottom">
         <p>
         New around here? <a href='/register'>Sign up!</a>
         </p>
--- src/components/Register.js
+++ src/components/Register.js
@@ -11,32 +11,47 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    try {
-      const response = await fetch('http://localhost:5000/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
-      });
-      
-      if (!response.ok) {
-        throw new Error('Invalid username or password');
+
+    // Check if the form inputs are empty
+    var inputs = document.querySelectorAll('input');
+    var isValid = true;
+
+    inputs.forEach((input) => {
+        if (input.type !== 'submit' && input.value.trim() === '') {
+            setError('Please fill out the ' + input.id + ' field.');
+            isValid = false;
+            input.focus();
+            return false;
+        }
+    });
+
+    if (isValid) {
+      try {
+        const response = await fetch('http://localhost:5000/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ username, password })
+        });
+        
+        if (!response.ok) {
+          throw new Error('Invalid username or password');
+        }
+        
+        // Redirect the user to the home page or another page
+        navigate('/login');
+      } catch (error) {
+        setError('Invalid username or password');
+        console.error('Error logging in:', error);
       }
-      
-      // Redirect the user to the home page or another page
-      navigate('/login');
-    } catch (error) {
-      setError('Invalid username or password');
-      console.error('Error logging in:', error);
     }
   };
 
   return (
     <div className='register_box'>
       <img src={logo} alt="Logo"></img>
-      <form class="form" onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username:</label>
           <input
@@ -55,11 +70,11 @@ const Register = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {error && <p style={{ color: 'red', fontWeight: 500 }}>{error}</p>}
         <button type="submit">Create Account</button>
       </form>
 
-      <div class="bottom_elem">
+      <div className="bottom_elem">
         <p>
         You must be of legal drinking age in your country to join BeerReview.
         By clicking Create Account, you agree to our <button>Terms of Use</button> and our <button>Privacy Policy</button>.
